602eaf2eef13b0c8f166c5c6920f37b274fda98f
ali-ammar-dow <156888492+ali-ammar-dow@users.noreply.github.com>
2024-07-15 17:10:48 +0300

    Add files via upload

---
+++ kingfish_mail_template/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2018 ACSONE SA/NV
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "King Fish",
+    "summary": """ """,
+    "version": "0.1",
+    "license": "LGPL-3",
+    "author": "",
+    "website": "",
+    "depends": ["base",'website','crm'],
+    "data": [
+        'data/mail_template_crm_lead.xml',
+
+    ],
+
+}
